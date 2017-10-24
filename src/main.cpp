d207e3798d43b849d5dbdf8f68a5c31695c76a1b
Victor Glindås <glinkis@gmail.com>
2017-10-24 14:07:42 +0200

    Update main.cpp
    Changes instantiation into zero argument form.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include "base/Vector2.h"
 
 int main() {
-  Entity entity();
+  Entity entity;
   std::cout << entity->getId() << "\n";
 
-  Vector2 vector();
+  Vector2 vector;
   std::cout << vector->getId() << "\n";
 
   std::cout << "end" << "\n";
