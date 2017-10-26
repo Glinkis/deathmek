33b2b048621a1220ca72697ec5d24397ec015fb7
Victor Glindås <glinkis@gmail.com>
2017-10-26 20:49:37 +0200

    Implements missing overloads.

--- src/core/Vector2.cpp
+++ src/core/Vector2.cpp
@@ -12,3 +12,50 @@ bool Vector2::operator==(const Vector2 &vector) const
 {
   return x == vector.x && y == vector.y;
 }
+
+bool Vector2::operator!=(const Vector2 &vector) const
+{
+  return x != vector.x && y != vector.y;
+}
+
+Vector2 Vector2::operator+(const Vector2 &vector) const
+{
+  return Vector2(x + vector.x, y + vector.y);
+}
+
+Vector2 Vector2::operator+(double amount) const
+{
+  return Vector2(x + amount, y + amount);
+}
+
+Vector2 Vector2::operator-(const Vector2 &vector) const
+{
+  return Vector2(x - vector.x, y - vector.y);
+}
+
+Vector2 Vector2::operator-(double amount) const
+{
+  return Vector2(x - amount, y - amount);
+}
+
+Vector2 Vector2::operator*(const Vector2 &vector) const
+{
+  return Vector2(x * vector.x, y * vector.y);
+}
+
+Vector2 Vector2::operator*(double amount) const
+{
+  return Vector2(x * amount, y * amount);
+}
+
+Vector2 Vector2::operator/(const Vector2 &vector) const
+{
+  return Vector2(x / vector.x, y / vector.y);
+}
+
+Vector2 Vector2::operator/(double amount) const
+{
+  // Prevents problems with division by zero.
+  const double scale = 1. / amount;
+  return Vector2(x * scale, y * scale);
+}
