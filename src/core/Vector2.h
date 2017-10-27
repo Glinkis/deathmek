9987624cd21c343543cd7a2e159862d0b1e3f167
Victor Glindås <glinkis@gmail.com>
2017-10-27 08:41:39 +0200

    Update Vector2.h
    Replaces tabs with spaces.

--- src/core/Vector2.h
+++ src/core/Vector2.h
@@ -13,98 +13,98 @@ public:
   ~Vector2();
 
   /**
-	 * Compares this vector against another for equality.
-	 *
-	 * @param vector - The vector to compare against.
-	 * @return true if the two vectors are equal, otherwise false.
-	 */
+   * Compares this vector against another for equality.
+   *
+   * @param vector - The vector to compare against.
+   * @return true if the two vectors are equal, otherwise false.
+   */
   bool operator==(const Vector2 &vector) const;
 
   /**
-	 * Compares this vector against another for inequality.
-	 *
-	 * @param vector - The vector to compare against.
-	 * @return true if the two vectors are not equal, otherwise false.
-	 */
+   * Compares this vector against another for inequality.
+   *
+   * @param vector - The vector to compare against.
+   * @return true if the two vectors are not equal, otherwise false.
+   */
   bool operator!=(const Vector2 &vector) const;
 
   /**
-	 * Gets the result of adding two vectors together.
-	 *
-	 * @param vector - The other vector to add to this.
-	 * @return The result of adding the vectors together.
-	 */
+   * Gets the result of adding two vectors together.
+   *
+   * @param vector - The other vector to add to this.
+   * @return The result of adding the vectors together.
+   */
   Vector2 operator+(const Vector2 &vector) const;
 
   /**
-	 * Gets the result of this vector + amount.
-	 *
-	 * @param amount - An amount to add to each component.
-	 * @return The result of this vector + amount.
-	 */
+   * Gets the result of this vector + amount.
+   *
+   * @param amount - An amount to add to each component.
+   * @return The result of this vector + amount.
+   */
   Vector2 operator+(double amount) const;
 
   /**
-	 * Gets the result of subtracting a vector from this one.
-	 *
-	 * @param vector - The other vector to subtract from this.
-	 * @return The result of the subtraction.
-	 */
+   * Gets the result of subtracting a vector from this one.
+   *
+   * @param vector - The other vector to subtract from this.
+   * @return The result of the subtraction.
+   */
   Vector2 operator-(const Vector2 &vector) const;
 
   /**
-	 * Gets the result of this vector - amount.
-	 *
-	 * @param amount - An amount to subtract from each component.
-	 * @return The result of this vector - amount.
-	 */
+   * Gets the result of this vector - amount.
+   *
+   * @param amount - An amount to subtract from each component.
+   * @return The result of this vector - amount.
+   */
   Vector2 operator-(double amount) const;
 
   /**
-	 * Gets the result of multiplying this vector by another.
-	 *
-	 * @param vector - The other vector to multiply this by.
-	 * @return The result of the multiplication.
-	 */
+   * Gets the result of multiplying this vector by another.
+   *
+   * @param vector - The other vector to multiply this by.
+   * @return The result of the multiplication.
+   */
   Vector2 operator*(const Vector2 &vector) const;
 
   /**
-	 * Gets the result of scaling the vector by an amount.
-	 *
-	 * @param amount - How much to scale the vector by.
-	 * @return The result of scaling this vector.
-	 */
+   * Gets the result of scaling the vector by an amount.
+   *
+   * @param amount - How much to scale the vector by.
+   * @return The result of scaling this vector.
+   */
   Vector2 operator*(double amount) const;
 
   /**
-	 * Gets the result of dividing this vector by another.
-	 *
-	 * @param vector - The other vector to divide this by.
-	 * @return The result of the division.
-	 */
+   * Gets the result of dividing this vector by another.
+   *
+   * @param vector - The other vector to divide this by.
+   * @return The result of the division.
+   */
   Vector2 operator/(const Vector2 &vector) const;
 
   /**
-	 * Gets the result of dividing the vector by a value.
-	 *
-	 * @param amount - How much to divide the vector by.
-	 * @return  The result of division on this vector.
-	 */
+   * Gets the result of dividing the vector by a value.
+   *
+   * @param amount - How much to divide the vector by.
+   * @return  The result of division on this vector.
+   */
   Vector2 operator/(double amount) const;
 
   /**
-	 * Adds another vector to this.
-	 *
-	 * @param vector - The other vector to add.
-	 * @return Copy of the vector after addition.
-	 */
+   * Adds another vector to this.
+   *
+   * @param vector - The other vector to add.
+   * @return Copy of the vector after addition.
+   */
   Vector2 operator+=(const Vector2 &vector);
 
   /**
-	 * Subtracts another vector from this.
-	 *
-	 * @param vector - The other vector to subtract.
-	 * @return Copy of the vector after subtraction.
-	 */
+   * Subtracts another vector from this.
+   *
+   * @param vector - The other vector to subtract.
+   * @return Copy of the vector after subtraction.
+   */
   Vector2 operator-=(const Vector2 &vector);
 };
