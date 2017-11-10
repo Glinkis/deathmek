df0428669c9cce88ffab3c9031bfed84df49500e
Victor Glindås <glinkis@gmail.com>
2017-11-10 21:53:29 +0100

    Adds CodeBlocks stuff.

---
+++ CodeBlocks/CMakeFiles/deathmek.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deathmek.dir/src/main.cpp.obj"
+  "deathmek.pdb"
+  "deathmek.exe"
+  "deathmek.exe.manifest"
+  "libdeathmek.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deathmek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CodeBlocks/CMakeFiles/deathmek.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for deathmek.
+# This may be replaced when dependencies are built.
